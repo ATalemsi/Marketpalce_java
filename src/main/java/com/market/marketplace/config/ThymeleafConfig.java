086e3cf8b8efeb66857dbb9108f05bc76aae49de
radia idelkadi <idelkadiradia@gmail.com>
2024-10-16 12:31:56 +0100

    MJ-64 Add front-end implementation to display product details

--- src/main/java/com/market/marketplace/config/ThymeleafConfig.java
+++ src/main/java/com/market/marketplace/config/ThymeleafConfig.java
@@ -1,13 +1,13 @@
 package com.market.marketplace.config;
 
+import org.thymeleaf.TemplateEngine;
+import org.thymeleaf.templatemode.TemplateMode;
+import org.thymeleaf.templateresolver.ServletContextTemplateResolver;
+
 import javax.servlet.ServletContext;
 import javax.servlet.ServletContextEvent;
 import javax.servlet.ServletContextListener;
 import javax.servlet.annotation.WebListener;
-import org.thymeleaf.TemplateEngine;
-import org.thymeleaf.templatemode.TemplateMode;
-import org.thymeleaf.templateresolver.ITemplateResolver;
-import org.thymeleaf.templateresolver.ServletContextTemplateResolver;
 
 /**
  * Thymeleaf configuration.
---
+++ src/main/java/com/market/marketplace/servlet/Product/GetProduct.java
@@ -0,0 +1,29 @@
+package com.market.marketplace.servlet.Product;
+
+import com.market.marketplace.config.ThymeleafConfig;
+import org.thymeleaf.TemplateEngine;
+import org.thymeleaf.context.WebContext;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class GetProduct extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        // Set the content type to HTML
+        response.setContentType("text/html;charset=UTF-8");
+
+        // Retrieve the Thymeleaf TemplateEngine instance from the servlet context
+        TemplateEngine templateEngine = (TemplateEngine) getServletContext().getAttribute(ThymeleafConfig.TEMPLATE_ENGINE_ATTR);
+
+        // Create a WebContext for the current request
+        WebContext context = new WebContext(request, response, getServletContext());
+
+        // Render the index.html template and write to response's writer
+        templateEngine.process("index", context, response.getWriter());
+    }
+}

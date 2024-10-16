f7686e116ad751d6c302bf1096822f3cee83b36a
radia idelkadi <idelkadiradia@gmail.com>
2024-10-16 12:30:43 +0100

    MJ-64 Add front-end implementation to display product details

---
+++ src/main/java/com/market/marketplace/config/ThymeleafConfig.java
@@ -0,0 +1,41 @@
+package com.market.marketplace.config;
+
+import javax.servlet.ServletContext;
+import javax.servlet.ServletContextEvent;
+import javax.servlet.ServletContextListener;
+import javax.servlet.annotation.WebListener;
+import org.thymeleaf.TemplateEngine;
+import org.thymeleaf.templatemode.TemplateMode;
+import org.thymeleaf.templateresolver.ITemplateResolver;
+import org.thymeleaf.templateresolver.ServletContextTemplateResolver;
+
+/**
+ * Thymeleaf configuration.
+ */
+
+@WebListener
+public class ThymeleafConfig implements ServletContextListener {
+
+    public static final String TEMPLATE_ENGINE_ATTR = "com.thymeleafexamples.thymeleaf3.TemplateEngineInstance";
+
+    @Override
+    public void contextInitialized(ServletContextEvent sce) {
+        ServletContext servletContext = sce.getServletContext();
+
+        ServletContextTemplateResolver templateResolver = new ServletContextTemplateResolver(servletContext);
+        templateResolver.setTemplateMode(TemplateMode.HTML);
+        templateResolver.setPrefix("/WEB-INF/templates/");
+        templateResolver.setSuffix(".html");
+        templateResolver.setCacheTTLMs(3600000L);
+
+        TemplateEngine templateEngine = new TemplateEngine();
+        templateEngine.setTemplateResolver(templateResolver);
+
+        servletContext.setAttribute(TEMPLATE_ENGINE_ATTR, templateEngine);
+    }
+
+    @Override
+    public void contextDestroyed(ServletContextEvent sce) {
+        // Nettoyage si nécessaire
+    }
+}
